0bc2e772e946968605cc251211ce4a545ef3ad2c
Rafael Sitompul <141505074+RAFAELSITOMPUL@users.noreply.github.com>
2024-04-17 18:35:04 +0700

    Add files via upload

---
+++ belajar-javascript/script.js
@@ -0,0 +1,21 @@
+// Penjumlahan
+const angka1 = 10;
+const angka2 = 5;
+const hasilPenjumlahan = angka1 + angka2;
+console.log(`Hasil penjumlahan: ${hasilPenjumlahan}`);
+
+// Pengurangan
+const hasilPengurangan = angka1 - angka2;
+console.log(`Hasil pengurangan: ${hasilPengurangan}`);
+
+// Perkalian
+const hasilPerkalian = angka1 * angka2;
+console.log(`Hasil perkalian: ${hasilPerkalian}`);
+
+// Pembagian
+const hasilPembagian = angka1 / angka2;
+console.log(`Hasil pembagian: ${hasilPembagian}`);
+
+// Sisa bagi (modulo)
+const hasilModulo = angka1 % angka2;
+console.log(`Sisa bagi: ${hasilModulo}`);
